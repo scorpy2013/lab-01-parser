2c80c427ceb24807bacca8fc597fdd2b94790047
scorpy2013 <61156928+scorpy2013@users.noreply.github.com>
2020-09-09 10:00:44 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,33 @@
-// Copyright 2018 Your Name <your_email>
+// Copyright 2020 Alexander <scorpy2013@gmail.com>
 
 #include <gtest/gtest.h>
 
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+TEST(Json, Example)
+{
+    Json object = Json::parse(R"(
+        {
+            "lastname" : "Ivanov",
+            "firstname" : "Ivan",
+            "age" : 25,
+            "islegal" : false,
+            "marks" : [
+                4,5,5,5,2,3
+            ],
+            "address" : {
+                "city" : "Moscow",
+                "street" : "Vozdvijenka"
+            }
+        }
+    )");
+    EXPECT_EQ(any_cast<std::string>(object["lastname"]), "Ivanov");
+    EXPECT_EQ(any_cast<bool>(object["islegal"]), false);
+    EXPECT_EQ(any_cast<double>(object["age"]), 25);
+
+    Json marks = any_cast<Json>(object["marks"]);
+    EXPECT_EQ(any_cast<double>(marks[0]), 4);
+    EXPECT_EQ(any_cast<double>(marks[1]), 5);
+
+    Json address = any_cast<Json>(object["address"]);
+    EXPECT_EQ(any_cast<std::string>(address["city"]), "Moscow");
+    EXPECT_EQ(any_cast<std::string>(address["street"]), "Vozdvijenka");
 }
