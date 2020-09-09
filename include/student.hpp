5ff0d01497637abcfc71e493ac1f94fcdc692215
scorpy2013 <61156928+scorpy2013@users.noreply.github.com>
2020-09-09 09:48:16 +0300

    Update and rename header.hpp to student.hpp

---
+++ include/student.hpp
@@ -0,0 +1,11 @@
+// Copyright 2018 Your Name <your_email>
+
+#ifndef INCLUDE_STUDENT_HPP_
+#define INCLUDE_STUDENT_HPP_
+struct Student {
+    std::string Name;
+    std::any Group;
+    double Avg;
+    std::any Debt;
+}
+#endif // INCLUDE_STUDENT_HPP_
