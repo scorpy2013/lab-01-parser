de324bbd730812de9c330cf7d62930b37b229c35
scorpy2013 <61156928+scorpy2013@users.noreply.github.com>
2020-09-12 20:49:49 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,47 +4,25 @@
 
 TEST(Json, Example)
 {
-    Json object = Json::parse(R"(
-    {
-  "items": [
-    {
-      "name": "Ivanov Petr",
-      "group": "1",
-      "avg": "4.25",
-      "debt": null
-    },
-    {
-      "name": "Sidorov Ivan",
-      "group": 31,
-      "avg": 4,
-      "debt": "C++"
-    },
-    {
-      "name": "Pertov Nikita",
-      "group": "IU8-31",
-      "avg": 3.33,
-      "debt": [
-        "C++",
-        "Linux",
-        "Network"
-      ]
-    }
-  ],
-  "_meta": {
-    "count": 3
-  }
+   Json object("{\"name\" : \"Sidorov Ivan\",\"group\" : 31,\"avg\" : 4,\"debt\" : \"C++\"}");
+    EXPECT_EQ(any_cast<std::string>(object["name"]), "Sidorov Ivan");
+    EXPECT_EQ(any_cast<int>(object["group"]), 31);
+    EXPECT_EQ(any_cast<int>(object["avg"]), 4);
+    EXPECT_EQ(any_cast<std::string>(object["debt"]), "C++ ");
 }
-    )");
-   // ?????????????????????????????????????????????????????????????
-    EXPECT_EQ(any_cast<std::string>(object["lastname"]), "Ivanov");
-    EXPECT_EQ(any_cast<bool>(object["islegal"]), false);
-    EXPECT_EQ(any_cast<double>(object["age"]), 25);
+TEST(Json, ExampleJson)
+{
+    Json object("{\"lastname\" : \"Ivanov\",\"firstname\" : \"Ivan\",\"age\" : 25,\"islegal\" : false,\"marks\" : [4,5,5,5,2,3],\"address\" : {\"city\" : \"Moscow\",\"street\" : \"Vozdvijenka\"}}");
 
-    Json marks = any_cast<Json>(object["marks"]);
-    EXPECT_EQ(any_cast<double>(marks[0]), 4);
-    EXPECT_EQ(any_cast<double>(marks[1]), 5);
+    EXPECT_EQ(std::any_cast<std::string>(object["lastname"]), "Ivanov");
+    EXPECT_EQ(std::any_cast<bool>(object["islegal"]), false);
+    EXPECT_EQ(std::any_cast<double>(object["age"]), 25);
 
-    Json address = any_cast<Json>(object["address"]);
-    EXPECT_EQ(any_cast<std::string>(address["city"]), "Moscow");
-    EXPECT_EQ(any_cast<std::string>(address["street"]), "Vozdvijenka");
+    Json marks = std::any_cast<Json >(object["marks"]);
+    EXPECT_EQ(std::any_cast<double>(marks[0]), 4);
+    EXPECT_EQ(std::any_cast<double>(marks[1]), 5);
+
+    Json address = std::any_cast<Json >(object["address"]);
+    EXPECT_EQ(std::any_cast<std::string>(address["city"]), "Moscow");
+    EXPECT_EQ(std::any_cast<std::string>(address["street"]), "Vozdvijenka");
 }
