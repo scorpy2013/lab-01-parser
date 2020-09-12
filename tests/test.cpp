05057fba0739bf85fb76bc50f2881dcda6729c41
scorpy2013 <61156928+scorpy2013@users.noreply.github.com>
2020-09-12 21:22:20 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -2,7 +2,7 @@
 
 #include <gtest/gtest.h>
 
-TEST(Json, Example)
+TEST(Json, Example1)
 {
    Json object("{\"name\" : \"Sidorov Ivan\",\"group\" : 31,\"avg\" : 4,\"debt\" : \"C++\"}");
     EXPECT_EQ(any_cast<std::string>(object["name"]), "Sidorov Ivan");
@@ -10,19 +10,22 @@ TEST(Json, Example)
     EXPECT_EQ(any_cast<int>(object["avg"]), 4);
     EXPECT_EQ(any_cast<std::string>(object["debt"]), "C++ ");
 }
-TEST(Json, ExampleJson)
+TEST(Json, Example2)
 {
-    Json object("{\"lastname\" : \"Ivanov\",\"firstname\" : \"Ivan\",\"age\" : 25,\"islegal\" : false,\"marks\" : [4,5,5,5,2,3],\"address\" : {\"city\" : \"Moscow\",\"street\" : \"Vozdvijenka\"}}");
-
-    EXPECT_EQ(std::any_cast<std::string>(object["lastname"]), "Ivanov");
-    EXPECT_EQ(std::any_cast<bool>(object["islegal"]), false);
-    EXPECT_EQ(std::any_cast<double>(object["age"]), 25);
-
-    Json marks = std::any_cast<Json >(object["marks"]);
-    EXPECT_EQ(std::any_cast<double>(marks[0]), 4);
-    EXPECT_EQ(std::any_cast<double>(marks[1]), 5);
-
-    Json address = std::any_cast<Json >(object["address"]);
-    EXPECT_EQ(std::any_cast<std::string>(address["city"]), "Moscow");
-    EXPECT_EQ(std::any_cast<std::string>(address["street"]), "Vozdvijenka");
+    Json object("{\"name\": \"Ivanov Petr\", \"group\": 1, \"avg\": 4.25, \"debt\": \"null\"}");
+    EXPECT_EQ(std::any_cast<std::string>(object["name"]), "Ivanov Petr");
+    EXPECT_EQ(any_cast<int>(object["group"]), 1);
+    EXPECT_EQ(any_cast<int>(object["avg"]), 4.25);
+    EXPECT_EQ(any_cast<std::string>(object["debt"]), "null");
+} 
+TEST(Json, Example3)
+{
+   Json object("{\"name\" : \"Pertov Nikita\",\"group\" : \"IU8-31\",\"avg\" : 3.33,\"debt\" :  [\"C++\", \"Linux\", \"Network\"]}");
+    EXPECT_EQ(any_cast<std::string>(object["name"]), "Pertov Nikita");
+    EXPECT_EQ(any_cast<std::string>(object["group"]), "IU8-31");
+    EXPECT_EQ(any_cast<int>(object["avg"]), 3.33
+    Json debts = std::any_cast<Json >(object["debt"]);
+    EXPECT_EQ(std::any_cast<std::string>(debts[0]), "C++");
+    EXPECT_EQ(std::any_cast<std::string>(debts[1]), "Linux");
+    EXPECT_EQ(std::any_cast<std::string>(debts[2]), "Network");
 }
