1c569a02ab01166936320d3d68cea28f6fd7e1a1
scorpy2013 <61156928+scorpy2013@users.noreply.github.com>
2020-09-12 21:37:56 +0300

    Update and rename student.hpp to json.hpp

---
+++ include/json.hpp
@@ -0,0 +1,25 @@
+// Copyright 2020 Alexander <scorpy2013@gmail.com>
+
+#endif // INCLUDE_STUDENT_HPP_
+#ifndef UNTITLED2_JSON_H
+#define UNTITLED2_JSON_H
+
+#pragma once
+#include <any>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <map>
+#include <cmath>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+class Json
+{
+
+public:
+
+};
+#endif 
