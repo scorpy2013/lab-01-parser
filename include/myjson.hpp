2fb591210bdbed15e0135659bbab6a4cd4de01d7
scorpy2013 <61156928+scorpy2013@users.noreply.github.com>
2020-09-21 18:38:21 +0200

    Update and rename json.hpp to myjson.hpp

---
+++ include/myjson.hpp
@@ -0,0 +1,50 @@
+//
+// Created by alexscorpy on 18.09.2020.
+//
+/*
+| name          | group  | avg  | debt          |
+|---------------|--------|------|---------------|
+| Ivanov Petr   | 1      | 4.25 | null          |
+|---------------|--------|------|---------------|
+| Sidorov Ivan  | 31     | 4.00 | C++           |
+|---------------|--------|------|---------------|
+| Pertov Nikita | IU8-31 | 3.33 | 3 items       |
+|---------------|--------|------|---------------|
+ */
+#ifndef TEMPLATE_MYJSON_HPP
+#define TEMPLATE_MYJSON_HPP
+
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "student.hpp"
+
+struct lengths_of_fields
+{
+  size_t length_for_name; // длина строчки для name
+  size_t length_for_group; // длина строчки для group
+  size_t length_for_avg; // длина строчки для avg
+  size_t length_for_debt; // длина строчки для debt
+};
+class MyJson
+{
+ private:
+  std::vector<Student> students; // вектор студентов
+  // изначальная длина полей для имени, группы, среднего балла и долгов
+  lengths_of_fields length{15, 8, 6, 15};
+  void set_lengths_of_fields(); // установка длин полей
+ public:
+  MyJson();
+  // конструктор для строки "путь"
+  explicit MyJson(const std::string &jsonPath);
+  ~MyJson(); // деструктор
+  void print_one_line(const Student &student) const; // рисуем 1 строчку в таблице
+  [[nodiscard]] std::string get_one_split() const; // создаем универсальный разделитель в таблице
+  void parser(const std::string &jsonPath); // функция "парсинга"
+};
+
+#endif  // TEMPLATE_MYJSON_HPP
