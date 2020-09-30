cb8c8c4c71bc0bfc7531252f098002fcc8a23cee
scorpy2013 <61156928+scorpy2013@users.noreply.github.com>
2020-09-30 20:25:37 +0200

    Update myjson.hpp

--- include/myjson.hpp
+++ include/myjson.hpp
@@ -33,18 +33,21 @@ struct lengths_of_fields
 class MyJson
 {
  private:
+ void set_lengths_of_fields(); // установка длин полей
+ public:
   std::vector<Student> students; // вектор студентов
   // изначальная длина полей для имени, группы, среднего балла и долгов
   lengths_of_fields length{15, 8, 6, 15};
-  void set_lengths_of_fields(); // установка длин полей
- public:
   MyJson();
-  // конструктор для строки "путь"
+  // конструктор для строки объекта json файла
   explicit MyJson(const std::string &jsonPath);
   ~MyJson(); // деструктор
+  // выводим длины полей
+  [[nodiscard]] const lengths_of_fields &get_length() const;
   void print_one_line(const Student &student) const; // рисуем 1 строчку в таблице
   [[nodiscard]] std::string get_one_split() const; // создаем универсальный разделитель в таблице
   void parser(const std::string &jsonPath); // функция "парсинга"
+  void set_json(const std::string &JSON); // "сеттер" для строки json файла
 };
-
 #endif  // TEMPLATE_MYJSON_HPP
+
