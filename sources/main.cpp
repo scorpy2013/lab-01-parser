350c7ba020b66cbd55998fc0e0c236a80154e495
scorpy2013 <61156928+scorpy2013@users.noreply.github.com>
2020-09-30 20:28:28 +0200

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,16 +1,41 @@
-int main() {
-    //...
-    std::ifstream file{jsonPath};
-    if (!file) {
-        throw std::runtime_error{"unable to open json: " + jsonPath};
+//
+// Created by alexscorpy on 28.09.2020.
+//
+#include "myjson.hpp"
+#include <iostream>
+using namespace std;
+int main()
+{
+  MyJson data;
+  std::string JSONFILE(R"({
+  "items": [
+    {
+      "name": "Alex Volodin Andreevich",
+      "group": "IU8-33 the best group forever",
+      "avg": "5.000",
+      "debt": null
+    },
+    {
+      "name": "Sidorov Ivan",
+      "group": 31,
+      "avg": 4,
+      "debt": "C++ Java Python C# Python"
+    },
+    {
+      "name": "Pertov Nikita",
+      "group": "IU8-31",
+      "avg": 3.33,
+      "debt": [
+        "C++",
+        "Linux",
+        "Network"
+      ]
     }
-
-    json data;
-    file >> data;
-
-    std::vector<Student> students;
-    for (auto const& student : data.at("items")) {
-        students.push_back(Student{student});
-    }
-    //...
+  ],
+  "_meta": {
+    "count": 3
+  }
+})");
+  data.parser(JSONFILE);
+  return 0;
 }
